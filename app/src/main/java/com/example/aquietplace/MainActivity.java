abde4b8ded587693f8d7d52e1d411b840b009f7f
Justine <jkylebeldad@gmail.com>
2024-05-11 22:17:02 +1000

    initial app design

---
+++ app/src/main/java/com/example/aquietplace/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.aquietplace;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.aquietplace.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home){
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.profile){
+                replaceFragment(new ProfileFragment());
+            } else if (item.getItemId() == R.id.settings) {
+                replaceFragment(new SettingsFragment());
+            }
+
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+}
